a5b81b7ab9143d615a6ad9feca85579db11404c8
s3bc40 <s3bc40@gmail.com>
2025-05-18 23:58:58 +0200

    feat: add navbar with logo icon and connect wallet btn

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next"
 import "./globals.css"
+import "@rainbow-me/rainbowkit/styles.css"
+import Link from "next/link"
+import AvatarBrandSvg from "@/ui/AvatarBrandSvg"
+import { Providers } from "@/app/providers"
+import { ConnectButton } from "@rainbow-me/rainbowkit"
 
 export const metadata: Metadata = {
   title: "s3bc40 portfolio",
@@ -13,7 +18,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-gray-900">
+        {/* Providers for Web3 configs */}
+        <Providers>
+          {/* Navbar */}
+          <nav className="flex w-full items-center justify-between gap-4 bg-gray-800 p-4">
+            {/* Icon top left */}
+            <Link href="/">
+              <AvatarBrandSvg className="h-10 w-10 fill-white transition-colors hover:fill-yellow-500" />
+            </Link>
+            {/* Connect button wallet */}
+            <ConnectButton />
+          </nav>
+          {/* Main content */}
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
---
+++ next-app/src/wagmi.config.ts
@@ -0,0 +1,21 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import {
+    anvil,
+    sepolia,
+    zksyncSepoliaTestnet,
+    mainnet,
+    zksync
+} from 'wagmi/chains';
+
+export const wagmiConfig = getDefaultConfig({
+    appName: 'Portfolio Web3',
+    projectId: 'YOUR_PROJECT_ID',
+    chains: [
+        anvil,
+        mainnet,
+        zksync,
+        zksyncSepoliaTestnet,
+        sepolia
+    ],
+    ssr: true,
+});
