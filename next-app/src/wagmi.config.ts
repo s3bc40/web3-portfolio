22b3c6888d3a71c8ef1ad807f4ac8868739d0187
s3bc40 <s3bc40@gmail.com>
2025-06-17 11:33:35 +0200

    feat: improve config to show anvil or not

--- next-app/src/wagmi.config.ts
+++ next-app/src/wagmi.config.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { http } from 'viem';
+import { http, HttpTransport } from 'viem';
 import {
     Config,
     cookieStorage,
@@ -10,22 +10,36 @@ import {
 import {
     anvil,
     zksyncSepoliaTestnet,
+    type Chain
 } from 'wagmi/chains';
+
+// Define your default chains that are always included (production and development)
+const defaultChains: [Chain, ...Chain[]] = [
+    zksyncSepoliaTestnet, // This is a public testnet, suitable for both dev and prod
+];
+
+// Conditionally add the Anvil chain only in development
+if (process.env.NODE_ENV === 'development') {
+    defaultChains.push(anvil);
+}
+
+// Helper function to create transports based on the chains array
+const createTransports = (chains: Chain[]) => {
+    const transports: { [key: number]: HttpTransport } = {};
+    chains.forEach(chain => {
+        // @dev use specific RPC URLs here in a real app,
+        //    but for now, we'll stick to http(). Using proxy to avoid exposing
+        //    RPC URLs in the client code.
+        transports[chain.id] = http();
+    });
+    return transports;
+};
 
 export default getDefaultConfig({
     appName: 'Portfolio Web3',
     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-    chains: [
-        anvil,
-        zksyncSepoliaTestnet,
-    ],
-    transports: {
-        // Transport configuration for each chain
-        // @dev not using custom RPC URLs, but in real-world applications, you would replace `http()` with your custom RPC URLs.
-        //    Since it is client-side, we would need to use a proxy to avoid exposing the RPC URLs.
-        [anvil.id]: http(),
-        [zksyncSepoliaTestnet.id]: http(),
-    },
+    chains: defaultChains,
+    transports: createTransports(defaultChains),
     ssr: true,
     // Persistence using Cookies
     storage: createStorage({
